f8146a8bc5ec3b5efc9ff2ad2dbfd108c6822fa0
Deva88 <devendra.raj.sdm@gmail.com>
2021-12-19 14:09:34 +0530

    Added: Local Variables

---
+++ variable/LocalExmaple.java
@@ -0,0 +1,10 @@
+package com.variable;
+
+public class LocalExmaple {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10; //local variables
+		int b=20;
+		System.out.println(a+b);	
+	}
+}
