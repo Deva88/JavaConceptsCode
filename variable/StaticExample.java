5558b1f2b6a71467df5ebcf1a1ea32471a073299
Deva88 <devendra.raj.sdm@gmail.com>
2021-12-19 14:16:23 +0530

    Added: Static Variables

---
+++ variable/StaticExample.java
@@ -0,0 +1,14 @@
+package com.variable;
+
+public class StaticExample {
+	//static var
+	static int num1=1000;
+	static int num2=2000;
+	//static method
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//static area
+		System.out.println(StaticExample.num1);
+		System.out.println(StaticExample.num2);
+	}
+}
