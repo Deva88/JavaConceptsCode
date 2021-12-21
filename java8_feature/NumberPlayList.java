abe4affe7a1a3c9663e415879f3e7ac89e556666
Deva88 <devendra.raj.sdm@gmail.com>
2021-12-21 10:17:08 +0530

    Added: java 8 feature concepts example

--- java8_feature/NumberPlayList.java
+++ java8_feature/NumberPlayList.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.*;
-
 public class NumberPlayList {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
